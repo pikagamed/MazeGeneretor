02a1f93ff0643d8ad39530f115c3f24b516829b8
pikagamed <51389229+pikagamed@users.noreply.github.com>
2019-07-24 16:49:51 +0800

    20190724 COMMIT

---
+++ MazeGeneretor/Assets/Boxcell.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boxcell
+{
+    #region 欄位宣告
+    protected int _xPosition;
+    protected int _yPosition;
+    private int _wallId;     //方格所在位置的x座標、y座標，不同為奇數或偶數時，為牆壁。 這個牆壁是可能被破壞的。如果不是牆壁，值為-1
+    private int[] _connectSpace = new int[2];   //該牆壁連接的兩個方格。如果該牆壁為邊界，兩值均為-1
+    private bool _wallOpen; //這個牆壁是否會出現。如果此值為true，表示在生成牆壁時，這個BOX的位置是空的。
+                                            //不能被破壞的牆壁為：x座標、y座標均為偶數；x座標或y座標為0或座標最大值(迷宮邊界)
+    private int _spaceId;  //方格所在位置的x座標、y座標均為奇數時，為空間。是玩家可以自由走動的地方。如果不是空間，值為-1
+    #endregion
+
+    #region 屬性設定
+    public int xPosition { get => _xPosition; }
+    public int yPosition { get => _yPosition; }
+    public int wallId { get => _wallId; }
+    public int[] connectSpace { get => _connectSpace; }
+    public bool wallOpen { get => _wallOpen; set => _wallOpen = value; }
+    public int spaceId { get => _spaceId; }
+    #endregion
+
+    #region 建構函式
+    public Boxcell(int xPos, int yPos, int wallNum, int connectSpace0, int connectSpace1, int spaceNum )
+    {
+        _xPosition = xPos;
+        _yPosition = yPos;
+        _wallId = wallNum;
+        _connectSpace[0] = connectSpace0;
+        _connectSpace[1] = connectSpace1;
+        _wallOpen = false;
+        _spaceId = spaceNum;
+    }
+    #endregion
+
+    #region 演算函式
+
+    #endregion
+}
---
+++ MazeGeneretor/Assets/MazeGenerator.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGenerator : MonoBehaviour
+{
+    [Header("固定出現的阻擋器")]
+    public GameObject cornerBox;
+    [Header("動態出現的阻擋器")]
+    public GameObject dynamicBox;
+    [Header("迷宮長"), Range(9,25)]
+    public int mazeHeight = 15;
+    int actualMazeHeight;
+    [Header("迷宮寬"), Range(9, 25)]
+    public int mazeWidth = 15;
+    int actualMazeWidth;
+    Boxcell[,] generateCell;
+    int spaceCellCount;
+    List<Boxcell> wallList;
+    List<List<int>> connectList = new List<List<int>>();   //巡迴導覽座標，這個List紀錄能巡覽到哪個Cell格。只要巡覽到該Cell，就認為這兩個Cell是連接的。
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region 基礎牆壁宣告
+        //迷宮實際上長寬為 給定數字×2+1.
+        //X邊界編號為mazeWidth×2、Y邊界編號為mazeHeight×2
+        actualMazeHeight = mazeHeight * 2 + 1;
+        actualMazeWidth = mazeWidth * 2 + 1;
+
+        spaceCellCount = 0;
+        wallList = new List<Boxcell>();
+        generateCell = new Boxcell[actualMazeHeight, actualMazeWidth];
+
+        for(int i=0; i< actualMazeHeight; i++)
+        {
+            for(int j=0; j< actualMazeWidth; j++)
+            {
+                //如果i和j都是偶數，或者i=0或邊界值，或者j=0或邊界值，此BOX為不可動的牆壁
+                //如果i是奇數，j是偶數，表示該BOX為連接左右空間之牆壁。 連接的空間為 [ (i/2)*mazeWidth+j/2-1, (i/2)*mazeWidth+j/2 ]
+                //如果i是偶數，j是奇數，表示該BOX為連接上下空間之牆壁。 連接的空間為 [ (i/2-1)*mazeWidth+j/2, (i/2)*mazeWidth+j/2 ]
+                //如果i和j都是奇數，則為空間
+
+                if(  (i%2==0 && j%2==0) || (i==0) || (i== actualMazeHeight-1) || (j==0) || (j== actualMazeWidth-1) )
+                {
+                    generateCell[i, j] = new Boxcell( i , j , -1, -1, -1, -1 );
+                }
+                else if( i%2==1 && j%2==0 )
+                {
+                    generateCell[i, j] = new Boxcell(i, j, wallList.Count, (i / 2) * mazeWidth + j / 2 - 1, (i / 2) * mazeWidth + j / 2, -1);
+                    wallList.Add(generateCell[i, j]);
+                }
+                else if (i % 2 == 0 && j % 2 == 1)
+                {
+                    generateCell[i, j] = new Boxcell(i, j, wallList.Count, (i / 2 - 1) * mazeWidth + j / 2, (i / 2) * mazeWidth + j / 2, -1);
+                    wallList.Add(generateCell[i, j]);
+                }
+                else
+                {
+                    generateCell[i, j] = new Boxcell(i, j, -1, -1, -1, spaceCellCount);
+                    connectList.Add(new List<int>());
+                    spaceCellCount++;
+                }
+            }
+        }
+        #endregion
+
+        #region 隨機打掉牆壁
+        while (wallList.Count > 0)
+        {
+            int drawWallId = Random.Range(0, wallList.Count);
+            int connectSpace0 = wallList[drawWallId].connectSpace[0];
+            int connectSpace1 = wallList[drawWallId].connectSpace[1];
+            //巡覽從connectSpace0到connectSpace1，如果是連接在一起的，不打掉牆壁，反之，打掉牆壁
+
+            if (!ExploreTo (connectSpace0, connectSpace1))
+            {
+                wallList[drawWallId].wallOpen = true;
+                connectList[connectSpace0].Add(connectSpace1);
+            }
+
+            wallList.Remove(wallList[drawWallId]);
+        }
+
+        #endregion
+
+
+        #region 迷宮生成
+        //正中央為(0,0)，左上角座標為( -0.75F*mazeWidth , 0.75F*mazeHeight )
+        for (int i=0; i<actualMazeHeight; i++)
+        {
+            for(int j=0; j<actualMazeWidth; j++)
+            {
+                if(generateCell[i, j].wallId == -1 && generateCell[i, j].spaceId==-1)
+                {
+                    Instantiate(cornerBox , new Vector2( -0.75F*mazeWidth + 0.75F*j, 0.75F*mazeHeight - 0.75F*i ), Quaternion.identity);
+                }
+                else if (generateCell[i, j].wallId != -1 && !generateCell[i,j].wallOpen)
+                {
+                    Instantiate(dynamicBox, new Vector2(-0.75F * mazeWidth + 0.75F * j, 0.75F * mazeHeight - 0.75F * i), Quaternion.identity);
+                }
+            }
+        }
+
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //巡覽
+    public bool ExploreTo(int startSpaceId, int targetSpaceId)
+    {
+        bool spaceFound = false;
+
+        for (int i = 0; i < connectList[startSpaceId].Count; i++)
+        {
+            //如果已經有找到，不要再浪費時間在迴圈中
+            if (spaceFound) return spaceFound;
+
+            if (connectList[startSpaceId][i] == targetSpaceId)
+            {
+                spaceFound = true;
+            }
+            else
+            {
+                spaceFound = ExploreTo(connectList[startSpaceId][i], targetSpaceId);
+                if (spaceFound) return spaceFound;
+                spaceFound = ExploreTo(targetSpaceId, connectList[startSpaceId][i]);
+                if (spaceFound) return spaceFound;
+            }
+        }
+
+        return spaceFound;
+    }
+}
